378dce95d48b46ba2d04546cb62251732f63e9b5
Kaushal1011 <kaushalpatil10@gmail.com>
2020-12-27 15:44:08 +0530

    final edit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,43 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import re
+from urllib.parse import urlsplit
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.urls import re_path
+from django.views.static import serve
+
+from django.urls import path
+
+
+def static(prefix, view=serve, **kwargs):
+    """
+    Return a URL pattern for serving files in debug mode.
+
+    from django.conf import settings
+    from django.conf.urls.static import static
+
+    urlpatterns = [
+        # ... the rest of your URLconf goes here ...
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    """
+    if not prefix:
+        raise ImproperlyConfigured("Empty static prefix not permitted")
+    elif urlsplit(prefix).netloc:
+        # No-op if not in debug mode or a non-local prefix.
+        return []
+    return [
+        re_path(r'^%s(?P<path>.*)$' %
+                re.escape(prefix.lstrip('/')), view, kwargs=kwargs),
+    ]
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('backend_app.urls'))
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
